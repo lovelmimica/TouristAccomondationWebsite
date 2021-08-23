c64652ea27017e4ce8252e049646fe3b027866ca
lovelmimica <lovel.mimica@gmail.com>
2021-08-23 19:21:17 +0200

    Updates

---
+++ precache-manifest.e27adfa448ad504da9c8edab2f6eecd7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc012b16c219fac458e585c5d4e21ea9",
+    "url": "/tourist-accomondation-website-preview/index.html"
+  },
+  {
+    "revision": "69b3ca8d4512026a7ce0",
+    "url": "/tourist-accomondation-website-preview/static/css/2.3d1ee733.chunk.css"
+  },
+  {
+    "revision": "c256ae5759c4c0130849",
+    "url": "/tourist-accomondation-website-preview/static/css/main.0a51452f.chunk.css"
+  },
+  {
+    "revision": "69b3ca8d4512026a7ce0",
+    "url": "/tourist-accomondation-website-preview/static/js/2.2271ca38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tourist-accomondation-website-preview/static/js/2.2271ca38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c256ae5759c4c0130849",
+    "url": "/tourist-accomondation-website-preview/static/js/main.f80b608b.chunk.js"
+  },
+  {
+    "revision": "226ec598dd1ddc3269c4",
+    "url": "/tourist-accomondation-website-preview/static/js/runtime-main.eee42c20.js"
+  },
+  {
+    "revision": "e45bc0877e4c0ffb5b5b45c413a26725",
+    "url": "/tourist-accomondation-website-preview/static/media/CharterBT.e45bc087.ttf"
+  },
+  {
+    "revision": "66e7bb9181f6f71e6bd763302d1b4f3e",
+    "url": "/tourist-accomondation-website-preview/static/media/beach.66e7bb91.jpeg"
+  },
+  {
+    "revision": "83318545aef3a88a0a63f0f8a8561eaa",
+    "url": "/tourist-accomondation-website-preview/static/media/dashed-border.83318545.png"
+  },
+  {
+    "revision": "6b3d70fb8436d0b1f0e9a3b742de3ef8",
+    "url": "/tourist-accomondation-website-preview/static/media/enjoy.6b3d70fb.png"
+  },
+  {
+    "revision": "81c584f714ff99ce747a940e176b8a4a",
+    "url": "/tourist-accomondation-website-preview/static/media/everything-you-need.81c584f7.jpg"
+  },
+  {
+    "revision": "4cef155aa201fc78775ffbcaa94cd40d",
+    "url": "/tourist-accomondation-website-preview/static/media/footer-background.4cef155a.jpg"
+  },
+  {
+    "revision": "c6e1a6b292f57911a33beb2e0b4290a0",
+    "url": "/tourist-accomondation-website-preview/static/media/gallery-1.c6e1a6b2.jpg"
+  },
+  {
+    "revision": "0dd543d2aa53ae0b0f46ab75609f4c4e",
+    "url": "/tourist-accomondation-website-preview/static/media/gallery-2.0dd543d2.jpg"
+  },
+  {
+    "revision": "0b3faae1c09a9e5e1fc14adbabe8f016",
+    "url": "/tourist-accomondation-website-preview/static/media/gallery-3.0b3faae1.jpg"
+  },
+  {
+    "revision": "c7901596d6bb76e55f840bb453410198",
+    "url": "/tourist-accomondation-website-preview/static/media/gallery-4.c7901596.jpg"
+  },
+  {
+    "revision": "42b4c0fc4e7ae99ed6fb21d0a5917e36",
+    "url": "/tourist-accomondation-website-preview/static/media/gallery-5.42b4c0fc.jpg"
+  },
+  {
+    "revision": "425abdccfa1522209f891f5066ccda17",
+    "url": "/tourist-accomondation-website-preview/static/media/gallery-6.425abdcc.jpg"
+  },
+  {
+    "revision": "d4644e366237ae0df85cac4037114874",
+    "url": "/tourist-accomondation-website-preview/static/media/interier.d4644e36.jpg"
+  },
+  {
+    "revision": "27cc5fef23647d6c1c6fa6f014fe325f",
+    "url": "/tourist-accomondation-website-preview/static/media/map-mask.27cc5fef.png"
+  },
+  {
+    "revision": "e0f1a8de8fcd17c2a4b53cb60cb19a47",
+    "url": "/tourist-accomondation-website-preview/static/media/private-and-clean.e0f1a8de.jpg"
+  },
+  {
+    "revision": "cc87f2949d850114b03c7da33bdbfefc",
+    "url": "/tourist-accomondation-website-preview/static/media/room-1.cc87f294.jpg"
+  },
+  {
+    "revision": "e8de2627e3fee48c4e2c4c6926729533",
+    "url": "/tourist-accomondation-website-preview/static/media/view.e8de2627.jpg"
+  }
+]);
